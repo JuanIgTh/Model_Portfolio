1c91cea2e17ec0b94524d9f37113f94a075fbed2
JuanIgTh <jit9rex@gmail.com>
2025-08-11 14:36:46 +0200

    A ver

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image'
 
 export default function HeroBanner() {
   return (
-    <section className="relative w-full h-[90vh] flex items-center justify-center">
-      <div className="relative w-full h-full ">
-        <Image
-          src="" // reemplazalo por tu PNG real
-          alt="Hero Banner"
-          fill
-          className="object-contain md:object-cover"
-          priority
-        />
+    <section className="relative w-full h-[80vh] flex items-center justify-center">
+    <div className="relative w-full h-full bg-black">
+      <Image
+        src="https://res.cloudinary.com/dmnwgau7y/image/upload/v1754915480/unnamed_jl2bsd.png"
+        alt="Hero Banner"
+        fill
+        className="md:object-cover"
+        priority
+      />
         {/* Overlay de texto opcional */}
         <div className="absolute inset-0 flex flex-col items-center justify-end pb-10 bg-gradient-to-b from-black via-transparent to-transparent">
           <h1 className="text-white text-3xl md:text-5xl font-light tracking-widest text-center">
